cb9b2a93128137fa3eb5c4e48778661ad005629d
Talouf <sylhorczak@gmail.com>
2023-09-16 08:46:57 +0200

    Adding match history with all the players and names + showing icons of items and summoner spells

--- client/src/components/PlayerSearch.js
+++ client/src/components/PlayerSearch.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function PlayerSearch() {
@@ -7,13 +7,36 @@ function PlayerSearch() {
     const [playerData, setPlayerData] = useState(null);
     const [lastMatch, setLastMatch] = useState(null);
     const [playerRank, setPlayerRank] = useState(null);
+    const [gameType, setGameType] = useState(""); // Game type filter
+    const [matchHistory, setMatchHistory] = useState([]); // Match history state
+    const [summonerSpellMapping, setSummonerSpellMapping] = useState({});
 
+    useEffect(() => {
+        async function fetchSummonerSpellMapping() {
+            try {
+                const response = await axios.get('https://ddragon.leagueoflegends.com/cdn/13.18.1/data/en_US/summoner.json');
+                const spells = response.data.data;
+                const mapping = {};
+
+                for (let key in spells) {
+                    mapping[spells[key].key] = key;
+                }
+
+                setSummonerSpellMapping(mapping);
+            } catch (error) {
+                console.error("Error fetching summoner spell data:", error);
+            }
+        }
+
+        fetchSummonerSpellMapping();
+    }, []);
     const fetchPlayerData = async () => {
         try {
             const response = await axios.get(`http://localhost:5000/api/players/${region}/summoner/${name}`);
-            setPlayerData(response.data.summoner); // Update this line to set the summoner data
-            fetchPlayerRank(response.data.summoner.id); // Fetch rank data using the summonerId
-            setLastMatch(response.data.lastMatch);
+            setPlayerData(response.data.summoner);
+            fetchPlayerRank(response.data.summoner.id);
+            // Assuming you have an endpoint for match history filtered by game type
+            fetchMatchHistory(response.data.summoner.puuid, gameType);
         } catch (error) {
             console.error("Error fetching player data:", error);
         }
@@ -22,11 +45,21 @@ function PlayerSearch() {
     const fetchPlayerRank = async (summonerId) => {
         try {
             const rankResponse = await axios.get(`http://localhost:5000/api/players/${region}/summoner/${summonerId}/rank`);
-            setPlayerRank(rankResponse.data[0]); // Assuming the data is an array and you're interested in the first entry
+            setPlayerRank(rankResponse.data[0]);
         } catch (error) {
             console.error("Error fetching rank data:", error);
         }
-    };    
+    };
+
+    const fetchMatchHistory = async (puuid, gameType) => {
+        try {
+            const matchHistoryResponse = await axios.get(`http://localhost:5000/api/players/${region}/summoner/${puuid}/matchhistory?gameType=${gameType}`);
+            setMatchHistory(matchHistoryResponse.data);
+            console.log("Match History:", matchHistoryResponse.data); // <-- Add this line
+        } catch (error) {
+            console.error("Error fetching match history:", error);
+        }
+    };
 
     const formatDuration = (seconds) => {
         const minutes = Math.floor(seconds / 60);
@@ -62,6 +95,7 @@ function PlayerSearch() {
                     <img src={`https://ddragon.leagueoflegends.com/cdn/13.18.1/img/profileicon/${playerData.profileIconId}.png`} alt={`${playerData.name}'s Profile Icon`} />
                 </div>
             )}
+
             {playerRank && (
                 <div>
                     <h3>Rank Details:</h3>
@@ -91,9 +125,45 @@ function PlayerSearch() {
                     </ul>
                 </div>
             )}
+
+            <div>
+                <h3>Match History:</h3>
+                <select value={gameType} onChange={(e) => setGameType(e.target.value)}>
+                    <option value="">All</option>
+                    <option value="ARAM">ARAM</option>
+                    <option value="CLASSIC">CLASSIC</option>
+                    {/* Add other game types as needed */}
+                </select>
+                <ul>
+                    {matchHistory.map((match, index) => (
+                        <li key={index}>
+                            {match.info.gameMode} - {formatDate(match.info.gameStartTimestamp)}
+                            <ul>
+                                {match.info.participants.map((participant, pIndex) => (
+                                    <li key={pIndex}>
+                                        <img src={`https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/${participant.championName}.png`} alt={`${participant.championName} Icon`} />
+                                        {Array(7).fill().map((_, itemIndex) => {
+                                            const itemId = participant[`item${itemIndex}`];
+                                            return itemId ? (
+                                                <img key={itemIndex} src={`https://ddragon.leagueoflegends.com/cdn/13.18.1/img/item/${itemId}.png`} alt={`Item ${itemId} Icon`} />
+                                            ) : null;
+                                        })}
+                                        <img src={`https://ddragon.leagueoflegends.com/cdn/13.18.1/img/spell/${summonerSpellMapping[participant.summoner1Id]}.png`} alt={`Summoner Spell ${participant.summoner1Id} Icon`} />
+                                        <img src={`https://ddragon.leagueoflegends.com/cdn/13.18.1/img/spell/${summonerSpellMapping[participant.summoner2Id]}.png`} alt={`Summoner Spell ${participant.summoner2Id} Icon`} />
+
+                                        {participant.summonerName} - {participant.championName}
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+
+
+
+            </div>
         </div>
     );
 }
 
 export default PlayerSearch;
-
--- server/routes/players.js
+++ server/routes/players.js
@@ -16,33 +16,36 @@ const fetchPlayerRank = async (summonerId, region) => {
     }
 };
 
-const fetchLastMatch = async (puuid, region) => {
+
+const fetchMatchHistory = async (puuid, region, gameType) => {
     const RIOT_BASE_URL = `https://${region}.api.riotgames.com/lol`;
     try {
-        const matchlistResponse = await axios.get(`https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=1&api_key=${RIOT_API_KEY}`);
-        const lastMatchId = matchlistResponse.data[0];
-        const matchDetailsResponse = await axios.get(`https://europe.api.riotgames.com/lol/match/v5/matches/${lastMatchId}?api_key=${RIOT_API_KEY}`);
-        return matchDetailsResponse.data;
+        const matchlistResponse = await axios.get(`https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=5&api_key=${RIOT_API_KEY}`);
+        const matchDetailsPromises = matchlistResponse.data.map(matchId => 
+            axios.get(`https://europe.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${RIOT_API_KEY}`)
+        );
+        const matchDetailsResponses = await Promise.all(matchDetailsPromises);
+        const matches = matchDetailsResponses.map(response => response.data);
+        return gameType ? matches.filter(match => match.info.gameMode === gameType) : matches;
     } catch (error) {
-        console.error("Error fetching last match:", error);
+        console.error("Error fetching match history:", error);
         return null;
     }
 };
 
 router.get('/:region/summoner/:name', async (req, res) => {
     const RIOT_BASE_URL = `https://${req.params.region}.api.riotgames.com/lol`;
+    const gameType = req.query.gameType;
 
     try {
         const encodedName = encodeURIComponent(req.params.name);
         const summonerResponse = await axios.get(`${RIOT_BASE_URL}/summoner/v4/summoners/by-name/${encodedName}?api_key=${RIOT_API_KEY}`);
-        
         const rankData = await fetchPlayerRank(summonerResponse.data.id, req.params.region);
-        const lastMatchData = await fetchLastMatch(summonerResponse.data.puuid, req.params.region);
-
+        const matchHistoryData = await fetchMatchHistory(summonerResponse.data.puuid, req.params.region, gameType);
         res.json({
             summoner: summonerResponse.data,
             rank: rankData,
-            lastMatch: lastMatchData
+            matchHistory: matchHistoryData
         });
     } catch (error) {
         res.status(500).json({ message: "Internal Server Error", error: error.message });
@@ -51,6 +54,7 @@ router.get('/:region/summoner/:name', async (req, res) => {
 
 // New endpoint to fetch rank by summoner ID
 router.get('/:region/summoner/:summonerId/rank', async (req, res) => {
+    console.log("Accessing match history for:", req.params.puuid);
     try {
         const rankData = await fetchPlayerRank(req.params.summonerId, req.params.region);
         if (rankData) {
@@ -58,6 +62,15 @@ router.get('/:region/summoner/:summonerId/rank', async (req, res) => {
         } else {
             res.status(404).json({ message: "Rank data not found" });
         }
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
+});
+router.get('/:region/summoner/:puuid/matchhistory', async (req, res) => {
+    const gameType = req.query.gameType;
+    try {
+        const matchHistoryData = await fetchMatchHistory(req.params.puuid, req.params.region, gameType);
+        res.json(matchHistoryData);
     } catch (error) {
         res.status(500).json({ message: "Internal Server Error", error: error.message });
     }
