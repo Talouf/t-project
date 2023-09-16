e76a1793ef448cbe69ae92968819dacfbef8a402
Talouf <sylhorczak@gmail.com>
2023-09-16 07:21:17 +0200

    Further adjustements to PlayerSearch.js and index.js, showing last match + ranked data

--- client/src/components/PlayerSearch.js
+++ client/src/components/PlayerSearch.js
@@ -1,121 +1,94 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function PlayerSearch() {
     const [name, setName] = useState("");
     const [region, setRegion] = useState("na1");
     const [playerData, setPlayerData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [version, setVersion] = useState("13.18.1");
-    const [championsData, setChampionsData] = useState({});
-    const [showDetails, setShowDetails] = useState(false); // State to toggle the detailed data visibility
-
-    useEffect(() => {
-        const fetchVersion = async () => {
-            try {
-                const response = await axios.get('http://ddragon.leagueoflegends.com/api/versions.json');
-                setVersion(response.data[0]);
-            } catch (error) {
-                console.error("Error fetching version data:", error);
-            }
-        };
-        fetchVersion();
-    }, []);
-
-    useEffect(() => {
-        const fetchChampionData = async () => {
-            try {
-                const response = await axios.get(`https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`);
-                let champData = {};
-                Object.values(response.data.data).forEach(champion => {
-                    champData[champion.key] = {
-                        name: champion.name,
-                        img: `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.image.full}`
-                    };
-                });
-                setChampionsData(champData);
-            } catch (error) {
-                console.error("Error fetching champion data:", error);
-            }
-        };
-        fetchChampionData();
-    }, [version]);
+    const [lastMatch, setLastMatch] = useState(null);
+    const [playerRank, setPlayerRank] = useState(null);
 
     const fetchPlayerData = async () => {
         try {
-            setLoading(true);
             const response = await axios.get(`http://localhost:5000/api/players/${region}/summoner/${name}`);
-            setPlayerData(response.data);
-            setLoading(false);
+            setPlayerData(response.data.summoner); // Update this line to set the summoner data
+            fetchPlayerRank(response.data.summoner.id); // Fetch rank data using the summonerId
+            setLastMatch(response.data.lastMatch);
         } catch (error) {
             console.error("Error fetching player data:", error);
-            setLoading(false);
         }
+    };
+
+    const fetchPlayerRank = async (summonerId) => {
+        try {
+            const rankResponse = await axios.get(`http://localhost:5000/api/players/${region}/summoner/${summonerId}/rank`);
+            setPlayerRank(rankResponse.data[0]); // Assuming the data is an array and you're interested in the first entry
+        } catch (error) {
+            console.error("Error fetching rank data:", error);
+        }
+    };    
+
+    const formatDuration = (seconds) => {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes} minutes ${remainingSeconds} seconds`;
+    };
+
+    const formatDate = (timestamp) => {
+        const date = new Date(timestamp);
+        return date.toLocaleString();
     };
 
     return (
         <div className="p-4">
             <div className="mb-4">
-                {/* Search Bar */}
                 <input
                     type="text"
                     placeholder="Enter player name"
-                    className="p-2 border rounded"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                 />
-                <select value={region} onChange={(e) => setRegion(e.target.value)} className="ml-2 p-2 border rounded">
+                <select value={region} onChange={(e) => setRegion(e.target.value)}>
                     <option value="na1">NA</option>
                     <option value="euw1">EUW</option>
                     <option value="eun1">EUNE</option>
                 </select>
-                <button onClick={fetchPlayerData} className="ml-2 p-2 bg-blue-500 text-white rounded">Search</button>
+                <button onClick={fetchPlayerData}>Search</button>
             </div>
 
             {playerData && (
                 <div>
-                    {/* Previous Year Ranked - Assuming you have such a data in playerData.prevYearRanked */}
-                    <h3 className="text-xl mb-2">{playerData.prevYearRanked}</h3>
+                    <h2>{playerData.name}</h2>
+                    <img src={`https://ddragon.leagueoflegends.com/cdn/13.18.1/img/profileicon/${playerData.profileIconId}.png`} alt={`${playerData.name}'s Profile Icon`} />
+                </div>
+            )}
+            {playerRank && (
+                <div>
+                    <h3>Rank Details:</h3>
+                    <p>Queue Type: {playerRank.queueType}</p>
+                    <p>Tier: {playerRank.tier}</p>
+                    <p>Rank: {playerRank.rank}</p>
+                    <p>League Points: {playerRank.leaguePoints}</p>
+                    <p>Wins: {playerRank.wins}</p>
+                    <p>Losses: {playerRank.losses}</p>
+                </div>
+            )}
 
-                    {/* Player Logo & Name */}
-                    <div className="flex items-center mb-4">
-                        <img src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${playerData.profileIconId}.png`} alt={`${playerData.name}'s Profile Icon`} className="mr-4" />
-                        <h2 className="text-xl">{playerData.name}</h2>
-                    </div>
-
-                    {/* Summary Button */}
-                    <button onClick={() => setShowDetails(!showDetails)} className="mb-4 bg-blue-500 text-white p-2 rounded">Summary</button>
-
-                    {/* Detailed Data - Only show if showDetails is true */}
-                    {showDetails && (
-                        <div className="flex">
-                            {/* This Year's Champions Played */}
-                            <div className="w-1/2 pr-4">
-                                <h4>Champions Played This Year:</h4>
-                                {/* Iterate over the champions and display them */}
-                            </div>
-
-                            {/* Match History */}
-                            <div className="w-1/2 pl-4">
-                                <h4>Match History:</h4>
-                                {/* Buttons for Queue Types */}
-                                <div className="mb-2">
-                                    <button className="p-2 m-1 bg-gray-300 rounded">All</button>
-                                    <button className="p-2 m-1 bg-gray-300 rounded">Ranked Solo</button>
-                                    <button className="p-2 m-1 bg-gray-300 rounded">Ranked Flex</button>
-                                    <button className="p-2 m-1 bg-gray-300 rounded">ARAM</button>
-                                </div>
-
-                                {/* Matches List - Display the last 20 games or as per the filter */}
-                            </div>
-                        </div>
-                    )}
-
-                    {/* Recently Played - Last 20 games */}
-                    <div>
-                        <h4 className="mb-2">Recently Played (Last 20 games):</h4>
-                        {/* Display the list of recent 20 games */}
-                    </div>
+            {lastMatch && (
+                <div>
+                    <h3>Last Match Played:</h3>
+                    <p>Game Mode: {lastMatch.info.gameMode}</p>
+                    <p>Duration: {formatDuration(lastMatch.info.gameDuration)}</p>
+                    <p>Map: {lastMatch.info.mapId}</p>
+                    <p>Timestamp: {formatDate(lastMatch.info.gameStartTimestamp)}</p>
+                    <h4>Players:</h4>
+                    <ul>
+                        {lastMatch.info.participants.map((participant, index) => (
+                            <li key={index}>
+                                {participant.summonerName} - {participant.championName}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             )}
         </div>
@@ -123,3 +96,4 @@ function PlayerSearch() {
 }
 
 export default PlayerSearch;
+
